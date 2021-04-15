9eef9d91b7329ad853d118ff4190af851f9af35d
karentschanz0 <karen@antistatique.net>
2021-04-15 17:32:52 +0200

    Add style

--- src/challenge-1/components/PokemonCard.tsx
+++ src/challenge-1/components/PokemonCard.tsx
@@ -2,27 +2,14 @@ import React, { useEffect, useState } from "react"
 
 export type PokemonCardProps = {
   name: string;
-  setFavorite: ( name: string) => void; 
 }
 
-const PokemonCard = ({name, setFavorite }: PokemonCardProps) => {
+const PokemonCard = ({name}: PokemonCardProps) => {
 
   return (
-    <tr>
-      <td>
-        <p>{name}</p>
-      </td>
-      <td>
-        <input 
-          type="radio" 
-          value={name} 
-          name="pokemon" 
-          onChange={ 
-            () => setFavorite(name)
-          }
-        />
-      </td>
-    </tr>
+    <div>
+      <p>{name}</p>
+    </div>
   )
 }
 
