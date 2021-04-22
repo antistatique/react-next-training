846358478f57e4a90ae7f7d4b8fa2dad3841d8be
karentschanz0 <karen@antistatique.net>
2021-04-22 14:15:27 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { render } from 'react-dom';
 
-import App from './challenge-2/App';
+import App from './challenge-3/App';
 
 const rootElement = document.getElementById('root');
 render(<App />, rootElement);
