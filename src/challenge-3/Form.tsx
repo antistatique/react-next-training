be3edff212e395e9ce7f8a593fa597a2abda2967
karentschanz0 <karen@antistatique.net>
2021-04-23 11:10:33 +0200

    Verification error onSubmit

--- src/challenge-3/Form.tsx
+++ src/challenge-3/Form.tsx
@@ -1,14 +1,18 @@
-import React from "react";
-import { useForm } from "react-hook-form";
+import React, { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-type Inputs = {
-  example: string,
-  exampleRequired: string,
+
+type FormData = {
+  lastName: string;
+  firstName: string;
+  email: string;
+  message: string;
 };
 
 const Form = (): JSX.Element => {
   const { register, formState: { errors }, handleSubmit } = useForm();
-  const onSubmit = (data: any) => alert(JSON.stringify(data));
+  
+  const onSubmit: SubmitHandler<FormData> = data => console.log(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-10">
@@ -21,18 +25,18 @@ const Form = (): JSX.Element => {
           placeholder="First name"
           className="border" 
         />
-        {errors.firstName && "First name is required"}
+        {errors.firstName && <span>First name is required</span>}
       </div>
 
       <div className="flex flex-col">
-        <label htmlFor="lastName">First name</label>
+        <label htmlFor="lastName">Last name</label>
         <input 
           id="lastName" 
           {...register("lastName", { required: true, pattern: /^[A-Za-z]+$/i }) } 
           placeholder="Last name"
           className="border" 
         />
-        {errors.lastName && "Last name is required"}
+        {errors.lastName && <span>Last name is required</span>}
       </div>
 
       <div className="flex flex-col">
@@ -43,16 +47,17 @@ const Form = (): JSX.Element => {
           placeholder="Email" 
           className="border"
         />
-        {errors.email && "Email is required"}
+        {errors.email && <span>Email is required</span>}
       </div>
 
       <div className="flex flex-col">
         <textarea 
-          {...register("message", { required: true, maxLength: 30 })} 
-          placeholder="Message"
+          {...register("message", {required: true, maxLength: 240})} 
+          placeholder="Message 240 caractères max"
           className="border"
         />
-        {errors.message && "Message is required"}
+        {errors.message && errors.message.type === "required" && <span>Message is required</span>}
+        {errors.message && errors.message.type === "maxLength" && <span>Max length exceeded</span> }
       </div>
 
       <input type="submit" />
@@ -60,5 +65,7 @@ const Form = (): JSX.Element => {
   );
 
 }
+
+
 
 export default Form;
