832b289fc8816fd2bb3c1d8305b88686858d2f93
karentschanz0 <karen@antistatique.net>
2021-05-20 14:47:29 +0200

    Add storybook

--- src/challenge-6/pages/_app.tsx
+++ src/challenge-6/pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/base.css'
+import '../styles/tailwind.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
